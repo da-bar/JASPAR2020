27a0ddece7ecaf0984e3046a88d20ae86cd97d58
Damir <damir.baranasic@imperial.ac.uk>
2019-08-31 08:50:55 -0400

    Initial commit

---
+++ R/AllClasses.R
@@ -0,0 +1,30 @@
+#' JASPAR2020 object class
+#'
+#' @description The JASPAR2020 object class is a thin class for storing the path of
+#' JASPAR2020 style SQLite file.
+#' @aliases JASPAR2020
+#' @slot db Object of class \code{"character"} a character string of the path
+#'  of SQLite file.
+#' @author Damir Baranasic
+#' @keywords classes
+#' @examples
+#' library(TFBSTools)
+#' opts <- list()
+#' opts[["species"]] <- 9606
+#' opts[["type"]] <- "SELEX"
+#' opts[["all_versions"]] <- TRUE
+#' PFMatrixList <- getMatrixSet(JASPAR2020placeholder, opts)
+#'
+#' opts2 <- list()
+#' opts2[["type"]] <- "SELEX"
+#' PFMatrixList2 <- getMatrixSet(JASPAR2020placeholder, opts2)
+#'
+#' @import methods
+#' @import JASPAR2014
+#' @import JASPAR2016
+#' @import JASPAR2018
+#' @exportClass JASPAR2020
+setClass("JASPAR2020", slots = c(db = "character")
+         )
+
+
---
+++ R/zzz.R
@@ -0,0 +1,12 @@
+.onLoad <- function(libname, pkgname){
+  ns <- asNamespace(pkgname)
+  path <- system.file("extdata", package=pkgname, lib.loc=libname)
+  files <- list.files(path, pattern="\\.sqlite$", full.names=TRUE)
+  #namespaceExport(ns, JASPAR2016)
+  for(i in seq_len(length(files))){
+    objname <- sub(".sqlite$","",basename(files[i]))
+    jasparDb <- new("JASPAR2020", db=files[i])
+    assign(objname, jasparDb, envir=ns)
+    namespaceExport(ns, objname)
+  }
+}
